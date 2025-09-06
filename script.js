330ead26d25ecbb0f3c09e5a9daa2226c99adb4b
Samuel Yahaya <yahayasamuel541@gmail.com>
2025-09-06 16:02:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,15 +17,70 @@ menuBtn.onclick = () => {
   grabCursor: true,
 });
 
-var swiperFood = new Swiper(".food-slider", {
+ var swiperFood = new Swiper( ".food-slider", {
   loop: true,
   grabCursor: true,
   centeredSlides: true,
+   pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+  },
+  breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          700: {
+            slidesPerView: 2,
+          },
+          1000: {
+            slidesPerView: 3,
+          },
+        },
+});
+    let previewContainer = document.querySelector('.food-preview-container');
+    let previewBox = previewContainer.querySelectorAll('.food-preview');
+
+    document.querySelectorAll('.food .swiper-slide').forEach((food, index) => {
+        food.onclick = () => {
+          previewContainer.style.display = 'flex';
+          let name = food.getAttribute('data-name');
+            previewBox.forEach((preview) => {
+                let target = preview.getAttribute('data-name');
+                if(name == target){ preview.classList.add('active'); }
+                
+            });
+        };
+    });
+    previewContainer.querySelectorAll('.fa-times').forEach(closeBtn => {
+      closeBtn.onclick = () => {
+        previewContainer.style.display = 'none';
+        previewBox.forEach(close => {
+          close.classList.remove('active');
+        });
+      };
+    });
+     var swiperMenu = new Swiper( ".menu-slider", {
+  loop: true,
+  autoHeight: true,
+  grabCursor: true,
+  centeredSlides: true,
+   pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+  },
+  
+});
+
+ var swiperBlog = new Swiper( ".blog-slider", {
+  loop: true,
+  autoHeight: true,
+  grabCursor: true,
+  centeredSlides: false,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  breakpoints: {
+    breakpoints: {
     0: {
       slidesPerView: 1,
     },
@@ -37,4 +92,3 @@ var swiperFood = new Swiper(".food-slider", {
     },
   },
 });
-    
